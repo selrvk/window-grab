5a5e890302c2a984b65164a7092854ad401e2caf
charles <charles.a7cantara@gmail.com>
2026-02-26 00:09:24 +0800

    added gesture readings and supporting visualization functions

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ import mediapipe as mp
 import cv2
 import numpy as np
 
-from utils import draw_landmarks_on_image
+from utils import draw_landmarks_on_image, display_batch_of_images_with_gestures_and_hand_landmarks, draw_gestures_on_image
 
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
-base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
-options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
-detector = vision.HandLandmarker.create_from_options(options)
+#base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
+#options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
+#detector = vision.HandLandmarker.create_from_options(options)
+
+base_options = python.BaseOptions(model_asset_path='gesture_recognizer.task')
+options = vision.GestureRecognizerOptions(base_options=base_options)
+recognizer = vision.GestureRecognizer.create_from_options(options)
+
+images = []
+results = []
 
 # Get camera frame
 cap = cv2.VideoCapture(0)
@@ -20,25 +27,24 @@ if not cap.isOpened():
   exit()
 
 while True:
-    
     ret, frame = cap.read()
-
-    if not ret:
-       print("Can't receive frame!")
-       break
+    if not ret: break
     
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb)
 
-    # static image file for testing (works)
-    #image = mp.Image.create_from_file("image.jpg")
+    recognition_result = recognizer.recognize(mp_image)
 
-    detection_result = detector.detect(mp_image)
-    annotated_image = draw_landmarks_on_image(mp_image.numpy_view(), detection_result)
+    if recognition_result.hand_landmarks:
+        annotated_image = draw_gestures_on_image(rgb, recognition_result)
+    else:
+        annotated_image = rgb
+
     final_bgr_image = cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR)
-    cv2.imshow('Hand Detection', final_bgr_image)
+    cv2.imshow('Gesture Recognition', final_bgr_image)
 
-    if cv2.waitKey(1) & 0xFF == 27:
+    #esc
+    if cv2.waitKey(1) & 0xFF == 27: 
         break
 
 cv2.destroyAllWindows()
--- utils.py
+++ utils.py
@@ -1,13 +1,60 @@
+#visualization utils
 import mediapipe as mp
 import numpy as np
+import math
 import cv2
 
+from matplotlib import pyplot as plt
+
+plt.rcParams.update({
+    'axes.spines.top': False,
+    'axes.spines.right': False,
+    'axes.spines.left': False,
+    'axes.spines.bottom': False,
+    'xtick.labelbottom': False,
+    'xtick.bottom': False,
+    'ytick.labelleft': False,
+    'ytick.left': False,
+    'xtick.labeltop': False,
+    'xtick.top': False,
+    'ytick.labelright': False,
+    'ytick.right': False
+})
+
+mp_hands = mp.tasks.vision.HandLandmarksConnections
+mp_drawing = mp.tasks.vision.drawing_utils
+mp_drawing_styles = mp.tasks.vision.drawing_styles
+
+def draw_gestures_on_image(rgb_image, recognition_result):
+    annotated_image = np.copy(rgb_image)
+    
+    gesture_list = recognition_result.gestures
+    hand_landmarks_list = recognition_result.hand_landmarks
+    
+    for idx in range(len(hand_landmarks_list)):
+
+        hand_landmarks = hand_landmarks_list[idx]
+        mp_drawing.draw_landmarks(
+            annotated_image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
+
+        if gesture_list:
+            gesture = gesture_list[idx][0]
+            title = f"{gesture.category_name} ({gesture.score:.2f})"
+            
+            height, width, _ = annotated_image.shape
+            x_text = int(hand_landmarks[0].x * width)
+            y_text = int(hand_landmarks[0].y * height) - 20
+            
+            cv2.putText(annotated_image, title, (x_text, y_text), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+    return annotated_image
 
 def draw_landmarks_on_image(rgb_image, detection_result):
-
-    mp_hands = mp.tasks.vision.HandLandmarksConnections
-    mp_drawing = mp.tasks.vision.drawing_utils
-    mp_drawing_styles = mp.tasks.vision.drawing_styles
   
     MARGIN = 10
     FONT_SIZE = 1
@@ -40,3 +87,48 @@ def draw_landmarks_on_image(rgb_image, detection_result):
                     FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
 
     return annotated_image
+
+def display_one_image(image, title, subplot, titlesize=16):
+    """Displays one image along with the predicted category name and score."""
+    plt.subplot(*subplot)
+    plt.imshow(image)
+    if len(title) > 0:
+        plt.title(title, fontsize=int(titlesize), color='black', fontdict={'verticalalignment':'center'}, pad=int(titlesize/1.5))
+    return (subplot[0], subplot[1], subplot[2]+1)
+
+def display_batch_of_images_with_gestures_and_hand_landmarks(images, results):
+    """Displays a batch of images with the gesture category and its score along with the hand landmarks."""
+
+    images = [image.numpy_view() for image in images]
+    gestures = [top_gesture for (top_gesture, _) in results]
+    multi_hand_landmarks_list = [multi_hand_landmarks for (_, multi_hand_landmarks) in results]
+
+    rows = int(math.sqrt(len(images)))
+    cols = len(images) // rows
+
+    FIGSIZE = 13.0
+    SPACING = 0.1
+    subplot=(rows,cols, 1)
+    if rows < cols:
+        plt.figure(figsize=(FIGSIZE,FIGSIZE/cols*rows))
+    else:
+        plt.figure(figsize=(FIGSIZE/rows*cols,FIGSIZE))
+
+    for i, (image, gestures) in enumerate(zip(images[:rows*cols], gestures[:rows*cols])):
+        title = f"{gestures.category_name} ({gestures.score:.2f})"
+        dynamic_titlesize = FIGSIZE*SPACING/max(rows,cols) * 40 + 3
+        annotated_image = image.copy()
+
+        for hand_landmarks in multi_hand_landmarks_list[i]:
+          mp_drawing.draw_landmarks(
+            annotated_image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
+
+        subplot = display_one_image(annotated_image, title, subplot, titlesize=dynamic_titlesize)
+
+    plt.tight_layout()
+    plt.subplots_adjust(wspace=SPACING, hspace=SPACING)
+    plt.show()
