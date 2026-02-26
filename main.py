a13fd90342a3ed33d83008aecb7c271793264321
charles <charles.a7cantara@gmail.com>
2026-02-26 20:36:03 +0800

    uh yeah

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import mediapipe as mp
 import cv2
 import numpy as np
+import pyautogui
 
 from utils import draw_landmarks_on_image, display_batch_of_images_with_gestures_and_hand_landmarks, draw_gestures_on_image
 
@@ -38,7 +39,43 @@ while True:
     #for data structure breakdown
     if recognition_result.gestures:
       top_gesture = recognition_result.gestures[0][0]
-      print(f"Gesture: {top_gesture.category_name} - Score: {top_gesture.score:.2f}")
+      
+      #print(f"Gesture: {top_gesture.category_name} - Score: {top_gesture.score:.2f}")
+
+      if top_gesture.category_name == "Pointing_Up":
+        pointing_up_img = cv2.imread('./pointing_up.jpeg')
+        pointing_up_img = cv2.resize(pointing_up_img, (480, 480))
+        cv2.imshow('Result Image', pointing_up_img)
+
+      elif top_gesture.category_name == "Closed_Fist":
+        closed_fist_img = cv2.imread('./closed_fist.jpg')
+        closed_fist_img = cv2.resize(closed_fist_img, (480, 480))
+        cv2.imshow('Result Image', closed_fist_img)
+        #try:
+        #  np_window = pyautogui.getActiveWindow()
+
+        #  if np_window:
+        #     np_window.moveTo(150,200)
+
+        #except Exception as e:
+        #  print(f"An error occurred: {e}")
+           
+      
+      elif top_gesture.category_name == "Victory":
+        victory_img = cv2.imread('./victory.webp')
+        victory_img = cv2.resize(victory_img, (480, 480))
+        cv2.imshow('Result Image', victory_img)
+
+      elif top_gesture.category_name == "ILoveYou":
+        ily_img = cv2.imread('./i_love_you.jpg')
+        ily_img = cv2.resize(ily_img, (480, 480))
+        cv2.imshow('Result Image', ily_img)
+
+      elif top_gesture.category_name == "Thumb_Up":
+        tu_img = cv2.imread('./thumbs_up.gif')
+        tu_img = cv2.resize(tu_img, (480, 480))
+        cv2.imshow('Result Image', tu_img)
+
 
     if recognition_result.hand_landmarks:
         annotated_image = draw_gestures_on_image(rgb, recognition_result)
